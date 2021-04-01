39e42754d9cc5c2aaab3a427e778475be9b7447d
Takumi Miyoshi <34400mt@gmail.com>
2021-04-01 21:50:52 +0900

    develop Ising class

--- Ising.h
+++ Ising.h
@@ -1,1 +1,20 @@
 #pragma once
+#include "isingConstant.h"
+#include "mersenne.h"
+#include <array>
+
+class Ising
+{
+private:
+	// storage of spin coordination
+	// true means spin-up, false means spin-down
+	std::array<bool, isingConstant::latticeSize> m_spins{};
+		
+public:
+	Ising();
+
+	void initializeSpin();
+
+	void flip(size_t index);
+	bool getValue(size_t index) const;
+};
---
+++ isingConstant.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace isingConstant
+{
+	constexpr int timeSize{ 16 };
+	constexpr int spaceSize{ 32 };
+
+	constexpr int latticeSize{ timeSize * spaceSize };
+}
